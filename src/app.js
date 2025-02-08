b1d14f2f48288d0e20d043f8ced9d45c62643ff8
Ajay yadav <callajay24@gmail.com>
2025-02-08 23:09:03 +0530

    Authentication using JWT & cookies , Encryption of password , data sanitisation and schema validation

--- src/app.js
+++ src/app.js
@@ -2,65 +2,72 @@ const express = require("express");
 const connectDB = require("./Config/Database");
 const app = express();
 const User = require("./models/User");
+const {validateSignUpData} = require("./Utils/validation");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middleware/auth");
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 app.use(express.json());
+app.use(cookieParser( ));
 
 app.post("/signUp" ,async (req,res) => {
-    const user = new User(req.body);
     try{
+        validateSignUpData(req.body);
+        const {firstName, lastName , emailId , password} = req.body;
+        const passwordHash = await bcrypt.hash(password , 10);
+        console.log(passwordHash);
+
+        const user = new User({
+            firstName, lastName, emailId , password : passwordHash
+        });
+        console.log(user);
         await user.save();
         res.send("Data saved successfully");
     }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
-    }
-    
+        res.status(400).send("Error hello:" + err.message);
+    }  
 });
 
-app.get("/user" , async (req,res) => {
-    const email = req.body.emailId;
-    console.log(email);
+app.post("/login" ,async (req,res) => {
     try{
-        const data = await User.find({emailId : email});
-        res.send(data);
+        const {emailId , password} = req.body;
+        const user = await User.findOne({emailId : emailId});
+        if(!user){
+            throw new Error("User is not present");
+        }
+        const isPasswordValid = await user.validatePassword(password);
+        if(isPasswordValid){
+            const token = await user.getJWT();
+
+            res.cookie("token" , token , {expires: new Date(Date.now() + 8*3600000),});
+            res.send("Login successfully");
+        }else{
+            throw new Error("Invalid credential");
+        }
     }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
-    }
-
-});
-app.get("/feed" , async (req,res) => {
-    try{
-        const data = await User.find({});
-        res.send(data);
-    }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
-    }
-
-});
-
-app.delete("/user" , async (req,res) => {
-    const userId = req.body.userId;
-    try{
-        const data = await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully");
-    }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
-    }
-
+        res.status(400).send("Error :" + err.message);
+    }  
 });
 
-app.patch("/user" , async (req,res) => {
-    const { emailId, ...updatedData } = req.body;
+app.get("/profile" , userAuth , async (req,res) => {
     try{
-        const data = await User.findOneAndUpdate({emailId}, updatedData);
-        res.send("User updated successfully");
+        const user = req.user;
+        res.send(user);
     }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
-    }
+        res.status(400).send("Error :" + err.message);
+    }  
+});
 
+app.post("/sendConnectionRequest", userAuth , (req,res) => {
+    const user = req.user;
+
+    res.send(user.firstName + " send the connection request");
 });
 
 connectDB().then(() => {
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -13,13 +15,26 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
     },
-    age: {
-        type: String,
-    },
-    gender: {
-        type: String,
-    }
-});
+    } ,   
+    {
+        timestamps: true,
+    });
+
+userSchema.methods.getJWT = async function () {
+    const user = this;
+
+    const token = await jwt.sign({ _id : user._id} , "DevAjay@123", {expiresIn: "1d"});
+
+    return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser , passwordHash);
+    return isPasswordValid;
+};
 
 const User = mongoose.model("User", userSchema);
 
